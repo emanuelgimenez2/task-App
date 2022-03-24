af31b36fd06eac17b81f1cc6d42e5f7d352b078b
emanuel gimenez <emanuelgimenez2@gmail.com>
2022-03-24 15:16:48 -0300

    inicio de estilos

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import { TodoItem } from './components/TodoItem';
 import { CreateTodoButton } from './components/CreateTodoButton';
 // import './App.css';
 
-const todos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALAA', completed: false },
-];
+
 
 function App() {
   return (
